e9b1418ea6d8049bcc6f87e7fba175fada69e7f6
Mariana Xavier <marianaxrp@gmail.com>
2022-06-27 11:26:30 -0300

    initial commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import { Avatar } from "./Avatar"
+import styles from "./Comment.module.css"
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/marianaxrp.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mariana Xavier</strong>
+              <time title="11 de Maio às 11:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            {" "}
+            <ThumbsUp size={24} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
